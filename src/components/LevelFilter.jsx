81503d4ad44d8b4f410fb24969877e7db6ec694b
Maksim Gulay <maksimgulaj@gmail.com>
2023-08-06 17:06:31 +0300

    deleate wrong files

--- src/components/LevelFilter.jsx
+++
@@ -1,9 +0,0 @@
-export const LevelFilter = () => {
-  return (
-    <select>
-      <option value="Beginer">Beginer</option>
-      <option value="Intemediate">Intemediate</option>
-      <option value="Advanced">Advanced</option>
-    </select>
-  );
-};
--- src/components/UserProfileCard/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import { DescriptionList } from './UserDescription/Description';
-
-export const ProfileCard = () => {
-  return (
-    <div className="profile">
-      <DescriptionList />
-    </div>
-  );
-};
